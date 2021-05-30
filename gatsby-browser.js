673055cb7afbdfe1765ab3ce3e6b3214a884a5c0
Momin Bin Shahid <MominBinShahid@gmail.com>
2021-05-30 11:48:03 +0500

    Added more details for offline

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,7 @@
 // ref: https://www.gatsbyjs.com/docs/how-to/performance/add-offline-support-with-a-service-worker/#displaying-a-message-when-a-service-worker-updates
 
+// for more details: https://github.com/gatsbyjs/gatsby/issues/9087
+
 // eslint-disable-next-line import/prefer-default-export
 export const onServiceWorkerUpdateReady = () => {
   // eslint-disable-next-line no-alert
