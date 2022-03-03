c6569d41f5cb6d26fdf42f259c5314388a0c1b27
Momin Bin Shahid <MominBinShahid@gmail.com>
2022-03-03 09:55:59 +0500

    fix: replace rocket emoji in service worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,7 +6,7 @@
 export const onServiceWorkerUpdateReady = () => {
   // eslint-disable-next-line no-alert
   const answer = window.confirm(
-    'This application has been updated 🚀. '
+    'This application has been updated. 🚀 '
         + 'Reload to display the latest version?',
   );
   if (answer === true) {
